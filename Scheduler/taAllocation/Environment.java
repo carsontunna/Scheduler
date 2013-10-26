d832181c5ab8b74019cfae921971359628faa239
Carson <carson@localhost>
2013-10-26 09:11:02 -0600

    Setup Eclipse Project Not working

---
+++ Scheduler/taAllocation/Environment.java
@@ -0,0 +1,22 @@
+package taAllocation;
+
+public class Environment {
+
+	public static Environment get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Solution currentSolution;
+
+	public void fromFile(String datafile) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Scheduler/taAllocation/Solution.java
@@ -0,0 +1,23 @@
+package taAllocation;
+
+public class Solution {
+
+	public Solution(String outfilename) {
+		// TODO Auto-generated constructor stub
+	}
+	public static Object Verbosity;
+	public static Object verbosity;
+	public boolean isComplete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public boolean isSolved() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public int getGoodness() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
